19ca46c6fd768bb6414ce7f113260660b538e49b
munj1 <yimunj931@gmail.com>
2023-03-08 07:29:21 +0900

    done

--- theme.config.js
+++ theme.config.js
@@ -1,42 +1,67 @@
 export default {
-  projectLink: 'https://github.com/velodrome-finance/',
-  docsRepositoryBase: 'https://github.com/velodrome-finance/docs/blob/master',
-  titleSuffix: ' – Velodrome Finance',
+  // projectLink: "https://github.com/velodrome-finance/",
+  // docsRepositoryBase: "https://github.com/velodrome-finance/docs/blob/master",
+  titleSuffix: " – Expand Exchange",
   nextLinks: true,
   prevLinks: true,
   search: true,
   unstable_stork: false,
   darkMode: true,
   font: false,
-  footer: true,
-  footerText: `MIT ${new Date().getFullYear()} © Velodrome Finance.`,
-  footerEditLink: `Edit this page on GitHub`,
+  footer: false,
+  footerText: `MIT ${new Date().getFullYear()} © Expand Exchange.`,
+  // footerEditLink: `Edit this page on GitHub`,
   logo: (
     <>
-      <img src="/velodrome.svg" alt="Velodrome Finance" style={{height: '30px', padding: '0 5px'}} />
-      <span className="mr-2 font-extrabold md:inline">Velodrome Finance</span>
-      <span className="text-gray-600 font-normal hidden md:inline">
-        The liquidity base-layer of the Optimism ecosystem.
-      </span>
+      {/* <img
+        src="/velodrome.svg"
+        alt="Expand Exchange"
+        style={{ height: "30px", padding: "0 5px" }}
+      /> */}
+      <span className="mr-2 font-extrabold md:inline">Expand Exchange</span>
+      {/* <span className="text-gray-600 font-normal hidden md:inline">
+        The liquidity base-layer of the Arbitrum ecosystem.
+      </span> */}
     </>
   ),
   head: (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="description" content="Velodrome Finance: The liquidity base-layer of the Optimism ecosystem." />
+      <meta
+        name="description"
+        content="Expand Exchange: The liquidity base-layer of the Arbitrum ecosystem."
+      />
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@VelodromeFi" />
-      <meta name="twitter:title" content="Velodrome Finance: The liquidity base-layer of the Optimism ecosystem." />
-      <meta name="twitter:description" content="Velodrome Finance is an automated market maker (AMM or simply, decentralized exchange) on Optimism L2 blockchain." />
-      <meta name="twitter:image" content="https://velodrome-docs.pages.dev/velodrome_banner.jpg" />
+      <meta
+        name="twitter:title"
+        content="Expand Exchange: The liquidity base-layer of the Arbitrum ecosystem."
+      />
+      <meta
+        name="twitter:description"
+        content="Expand Exchange is an automated market maker (AMM or simply, decentralized exchange) on Arbitrum L2 blockchain."
+      />
+      {/* <meta
+        name="twitter:image"
+        content="https://velodrome-docs.pages.dev/velodrome_banner.jpg"
+      /> */}
 
-      <meta property="og:title" content="Velodrome Finance: The liquidity base-layer of the Optimism ecosystem." />
-      <meta property="og:description" content="Velodrome Finance is an automated market maker (AMM or simply, decentralized exchange) on Optimism L2 blockchain." />
-      <meta property="og:image" content="https://velodrome-docs.pages.dev/velodrome_banner.jpg" />
+      <meta
+        property="og:title"
+        content="Expand Exchange: The liquidity base-layer of the Arbitrum ecosystem."
+      />
+      <meta
+        property="og:description"
+        content="Expand Exchange is an automated market maker (AMM or simply, decentralized exchange) on Arbitrum L2 blockchain."
+      />
+      {/* <meta
+        property="og:image"
+        content="https://velodrome-docs.pages.dev/velodrome_banner.jpg"
+      /> */}
 
-      <meta name="apple-mobile-web-app-title" content="Velodrome Finance" />
+      <meta name="apple-mobile-web-app-title" content="Expand Exchange" />
       <link rel="stylesheet" href="/fonts.css" />
     </>
-  )
-}
+  ),
+};
